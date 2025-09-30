dc0e238897a6554c3162788f719728c5fb7794eb
mwangaza12 <josephmwangaza1@gmail.com>
2025-09-30 16:22:28 +0300

    refactored code

--- components/header.tsx
+++ components/header.tsx
@@ -82,7 +82,7 @@ export const HeroHeader = () => {
                                     variant="outline"
                                     size="sm"
                                     className={cn(isScrolled && 'lg:hidden')}>
-                                    <Link href="#">
+                                    <Link href="signin">
                                         <span>Login</span>
                                     </Link>
                                 </Button>
@@ -90,7 +90,7 @@ export const HeroHeader = () => {
                                     asChild
                                     size="sm"
                                     className={cn(isScrolled && 'lg:hidden')}>
-                                    <Link href="#">
+                                    <Link href="signup">
                                         <span>Sign Up</span>
                                     </Link>
                                 </Button>
@@ -98,7 +98,7 @@ export const HeroHeader = () => {
                                     asChild
                                     size="sm"
                                     className={cn(isScrolled ? 'lg:inline-flex' : 'hidden')}>
-                                    <Link href="#">
+                                    <Link href="signup">
                                         <span>Get Started</span>
                                     </Link>
                                 </Button>
